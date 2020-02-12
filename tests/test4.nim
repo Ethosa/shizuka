52298286dd28dce80be67fd5b409125d57990872
Ethosa <social.ethosa@gmail.com>
2020-02-12 14:23:06 +0700

    macro `~` was fix

--- tests/test4.nim
+++ tests/test4.nim
@@ -4,3 +4,7 @@ from asyncdispatch import waitFor
 import shizuka
 
 var vk = AVk("88005553535", "qwerty", debug=true)
+
+echo waitFor vk~users.get(user_ids="akihayase")
+echo waitFor vk~users.get(user_ids=1)
+echo waitFor vk~users.get(user_ids=123123)
